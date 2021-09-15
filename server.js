93863d92a9b9157c4daf7e9784585d63d3b90f8d
BelalElolahi <belalelolahi003@gmail.com>
2021-09-14 17:06:05 -0700

    add movieAPI

--- server.js
+++ server.js
@@ -2,67 +2,107 @@
  const express = require('express');
  const app = express();
  const cors = require('cors');
+ const axios = require('axios');
  app.use(cors());
- const weatherData = require('./Data/weatherData.json');
-require('dotenv').config();
+ require('dotenv').config();
 const PORT = process.env.PORT;
 
 
-//Create a end point 
 
 
 
-app.get('/weather_data',(req,res)=>{
-     
-    
+
+
+//WitherEndPoint
+let handleWeather = async (req,res)=>{
+
     let lat1 = Number(req.query.lat);
     let lon1 = Number(req.query.lon);
-    
+    let city_name = req.query.city;
 
-    if(lat1&&lon1){
+    if (lat1&&lon1&&city_name){
 
-        let result = [];
-         function HasLatLon(element) {
-            return element.lat === lat1 && element.lon === lon1;
-          }
-         result.push( weatherData.find(HasLatLon));
-
-         console.log(result);
-
-
-         if (result.length>0)
-         {
-            let city = result[0];
-            console.log(city);
-              
-            let days = city.data.map(day=>{
-                return {
-                    day:day.valid_date,
-                    deccription:day.weather.description
-                }
-            });
-            /*let custmResponse ={
-                day:days,
-                name:city.city_name
-            }*/
-            res.status(200).json(days);
-
-         } else 
-         {
-            res.status(404).send("resources not found");
-         }
-
-        
-
-    }else{
-         res.status(400).send('please inter correct quiry');
-    }
+        let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city_name}&key=${process.env.WITHER_API_KEY}&lat=${lat1}&lon=${lon1}`;
   
+        let weatherAxiosData = await axios.get(url);
+        let weatherData = weatherAxiosData.data;
+        
+      
+        let cleaneData = weatherData.data.map(item =>{
+            return new ForCast (item.datetime,item.weather.description);
+        });
+         
+        console.log(weatherData.data);
+          res.status(200).json(cleaneData);
+
+    }
+    else {
+        res.status(404).send("resources not found");
+    }
+
+  
+}
+app.get('/weather',handleWeather);
+
+
+
+let handleMovies = async (req,res)=>{
+
+    let city = req.query.searchQuery;
+    console.log(city);
+
+    if (city){
+            
+        let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`;
+        
+  
+        let moviesAxiosData = await axios.get(url);
+        let moviesData = moviesAxiosData.data;
+        
+       console.log(moviesData.results);
+        let cleaneData = moviesData.results.map(item =>{
+            return new Movie (item.title,item.overview,item.vote_average,item.vote_count,item.image_url,item.popularity,item.release_date);
+        });
+         
+        console.log(moviesData.data);
+          res.status(200).json(cleaneData);
+
+    }
+    else {
+        res.status(404).send("resources not found");
+    }
+
+  
+}
+//movie Endpoint
+app.get('/movies',handleMovies);
+
+app.listen(PORT,()=>{
+    console.log("Listen in port"+PORT);
 });
 
 
- app.listen(PORT,()=>{
-      console.log(`we are listen : ${PORT}`)
- });
+//model
+class ForCast{
+    constructor(date,description){
+        this.date=date,
+        this.description=description
+    }
+}
 
+ 
+
+//model 
+class Movie {
+    constructor(title,overview,averageVotes,totalVotes,imageUrl,popularity,releasedOn){
+         this.title=title,
+         this.overview=overview,
+         this.averageVotes = averageVotes,
+         this.totalVotes= totalVotes,
+         this.imageUrl = imageUrl,
+         this.popularity= popularity,
+         this.releasedOn = releasedOn
+
+    }
+}
 
